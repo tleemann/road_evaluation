c1402bd002d417e14f6d7d6cedd6f2a9a3988104
Tobias Leemann <tobias.leemann@live.de>
2022-07-12 10:46:37 +0200

    Added install script for windows.

---
+++ setup.ps1
@@ -0,0 +1,4 @@
+conda create -n road
+conda activate road
+conda install -c pytorch python=3.8 torchvision=0.13 matplotlib=3.5 tqdm numpy=1.22 scipy=1.7 ipykernel # Base requirements
+ipython kernel install --user --name=road # make kernel available to jupyter lab / notebook
